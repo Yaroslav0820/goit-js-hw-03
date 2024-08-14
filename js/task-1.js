30125799474e1d084509611df5be08d83af4b6bd
Yaroslav Lomtev <79333072+Yaroslav0820@users.noreply.github.com>
2024-08-14 12:24:55 +0300

    task-3

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function slugify(title){
+// const titleLowe = title.toLowerCase();
+// const titleSplit = titleLowe.split(" ");
+// const titleJoin = titleSplit.join("-");
+//  return titleJoin;
+ return title.toLowerCase().split(" ").join("-");
+}
+
+
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function filterArray(numbers, value) {
+const number = [];
+
+for (let index = 0; index < numbers.length; index++) {
+    if ( numbers[index] > value){
+        number.push(numbers[index])
+    }
+    
+}
+return number;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
